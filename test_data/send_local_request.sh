45884798fc19196b2d4781e3c16bf15ab3782356
Joel Krebs <jokr@fb.com>
2018-02-16 17:41:11 +0000

    add unfurl example curl

--- test_data/send_local_request.sh
+++ test_data/send_local_request.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -H "Content-Type: application/json" -d @test_data/$1.json http://:5000/api/message_callback
+curl -H "Content-Type: application/json" -d @test_data/$2.json http://:5000/api/$1
