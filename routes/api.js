4e988d1dcfad33af91a7da0620a6ffdb6c6a0bbf
Joel Krebs <jokr@fb.com>
2018-03-14 13:28:44 +1100

    add icon response

--- routes/api.js
+++ routes/api.js
@@ -166,6 +166,7 @@ router.route('/unfurl_callback')
                     title: doc.name,
                     description: doc.content.toString().substring(0, 200),
                     privacy: doc.privacy === 'public' ? 'organization' : 'accessible',
+                    icon: `${process.env.BASE_URL}taaskly-icon.png`,
                     type: 'doc',
                   };
                   return {data, user};
@@ -220,6 +221,7 @@ router.route('/unfurl_callback')
                     privacy: 'organization',
                     type: 'task',
                     additional_data: additionalData,
+                    icon: `${process.env.BASE_URL}taaskly-icon.png`,
                   };
                   return {data, user};
                 });
