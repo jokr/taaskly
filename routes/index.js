b58d569d688a96b37ed8021a4bcb3b8f94f7c50a
Joel Krebs <jokr@fb.com>
2018-03-23 16:29:17 +0000

    move download url outside of auth barrier

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const logger = require('heroku-logger');
 const passport = require('passport');
+const pdf = require('html-pdf');
+const pug = require('pug');
 
 const admin = require('./admin');
 const api = require('./api');
@@ -61,6 +63,43 @@ const auth = basicAuth({
   challenge: true,
   realm: 'taaskly',
 });
+
+router.route('/download/:id')
+  .get((req, res, next) => db.models.document.findById(
+      req.params.id, {
+        include: [{model: db.models.user, as: 'owner'}],
+      },
+    )
+    .then(document => {
+      const html = pug.compileFile('./views/pdf.pug');
+      pdf
+        .create(
+          html({document}),
+          {
+            border: {
+              top: '15mm',
+              right: '10mm',
+              bottom: '15mm',
+              left: '10mm',
+            },
+            footer: {
+              height: '10mm',
+              contents: {
+                default: document.owner.username,
+              },
+            },
+          },
+        )
+        .toStream((err, stream) => {
+          if (err) {
+            return next(err);
+          }
+          res.attachment('taaskly-doc.pdf');
+          stream.pipe(res);
+        });
+    })
+    .catch(next),
+  );
 
 router.use('/api', api);
 router.use(auth);
