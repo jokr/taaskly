03228db00cc5db2a0d97097391b1bbd68857cb66
Michal Kuzba <michalkuzba@fb.com>
2019-11-15 11:56:27 +0000

    change mode for testing

--- routes/api/link.js
+++ routes/api/link.js
@@ -361,7 +361,7 @@ async function encodeTask(link, user, task) {
     return {
       link: link ? link : `${process.env.BASE_URL}/task/${task.id}`,
       title: task.title,
-      privacy: 'personalized',
+      privacy: 'organization',
       type: 'task',
       actions: actions,
       additional_data: additionalData,
